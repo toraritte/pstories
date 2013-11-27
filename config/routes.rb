2256db51b8ddc31d5ef80c6d36a97cc2e008804e
Attila Gulyas <toraritte@gmail.com>
2013-11-26 17:58:09 -1000

    Add nested routes (stories{lines})

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Megapoc::Application.routes.draw do
+
+  resources :stories do
+    resources :lines
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
