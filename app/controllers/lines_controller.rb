69f8cbd3bc52459d7ed33a78b3ff9fae79a33b6c
Attila Gulyas <toraritte@gmail.com>
2013-11-30 22:25:52 -1000

    Add "edit line" but index fix pending
    If story contains plot_item that has no character associated then
    it will throw an exception. (character_id: nil)

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -13,7 +13,9 @@ class LinesController < ApplicationController
   def update
     @story = Story[params[:story_id]]
     @line = @story.lines_dataset.first(id: params[:id])
-    
+    @line.character.update(params[:character].permit(:name))
+    @line.update(params[:line].permit(:plot_item,:dialogue_item))
+    redirect_to story_lines_path(@story)
   end
 
   def create
